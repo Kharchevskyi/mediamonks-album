b33d47db20347d3f7ed666e8b0e95301d2c7fc5c
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2019-04-21 22:21:12 +0300

    initial commit

---
+++ MediaMonks Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaMonks Photos
+//
+//  Created by Anton Kharchevskyi on 4/21/19.
+//  Copyright © 2019 Anton Kharchevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
